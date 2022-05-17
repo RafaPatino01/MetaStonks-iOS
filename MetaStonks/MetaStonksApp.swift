66578e5c5943edba1de26b90c1b45381959d13e6
RafaPatino01 <RafaPatino01@outlook.com>
2022-05-17 11:29:23 -0500

    added initial files

---
+++ MetaStonks/MetaStonksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaStonksApp.swift
+//  MetaStonks
+//
+//  Created by Rafael Patino on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MetaStonksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
